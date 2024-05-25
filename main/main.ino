63021998478d33b07678f68eab49614884de144f
wj461 <wj4wj461@gmail.com>
2024-05-25 09:26:15 +0800

    buzz work

--- main/main.ino
+++ main/main.ino
@@ -1,12 +1,14 @@
-
 #define ZPIN 2
 #define YPIN 14
 #define XPIN 15
+
+#define BUZZPIN 10
 
 void setup() {
   pinMode(ZPIN, INPUT_PULLUP);
   pinMode(XPIN, INPUT);
   pinMode(YPIN, INPUT);
+  pinMode(BUZZPIN, OUTPUT);
   Serial.begin(19200);           //  setup serial
 }
 
@@ -14,6 +16,7 @@ void loop() {
   int x = analogRead(XPIN);
   int y = analogRead(YPIN);
   int z = digitalRead(ZPIN);
+  tone(BUZZPIN, 1200);
   Serial.print(x + String(" "));
   Serial.print(y + String(" "));
   Serial.println(z);
